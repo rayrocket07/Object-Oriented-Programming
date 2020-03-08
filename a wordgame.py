a135855872f89c33f2b472c0e7e6b2f48ba83c1e
rayrocket07 <61902415+rayrocket07@users.noreply.github.com>
2020-03-08 18:24:48 +0700

    Update a wordgame.py

--- a wordgame.py
+++ a wordgame.py
@@ -86,6 +86,56 @@ def calculateHandlen(hand):
         total += i
     return total
 
+def playHand(hand, wordList, n):
+    """
+    Allows the user to play the given hand, as follows:
+
+    * The hand is displayed.
+    * The user may input a word or a single period (the string ".") 
+      to indicate they're done playing
+    * Invalid words are rejected, and a message is displayed asking
+      the user to choose another word until they enter a valid word or "."
+    * When a valid word is entered, it uses up letters from the hand.
+    * After every valid word: the score for that word is displayed,
+      the remaining letters in the hand are displayed, and the user
+      is asked to input another word.
+    * The sum of the word scores is displayed when the hand finishes.
+    * The hand finishes when there are no more unused letters or the user
+      inputs a "."
+
+      hand: dictionary (string -> int)
+      wordList: list of lowercase strings
+      n: integer (HAND_SIZE; i.e., hand size required for additional points)
+      
+    """
+    def displayH(hand):
+        handword = ""
+        for key,value in hand.items():
+            handword += key*int(value)
+        return(handword)
+    number = 0
+    for i in range(n):
+        number += 1
+    score = 0
+    
+    while True:
+        print("Current Hand: " + str(displayH(hand)))
+        gword = input('Enter word, or a "." to indicate that you are finished: ')
+        if gword == ".":
+            return("Goodbye! Total score: " + str(score) + " points.") 
+        else:
+             if not isValidWord(gword, hand, wordList):
+                 print("Invalid word, please try again.")
+                 continue
+             else:
+                 score += getWordScore(gword, n)
+                 print('"' + str(gword) + '" earned ' + str(getWordScore(gword, n)) + " points. Total: " + str(score) + " points")
+                 number -= len(gword)
+             if number == 0:
+                 return("Run out of letters. Total score: " + str(score) + " points.")
+        
+
+
                
                    
            
