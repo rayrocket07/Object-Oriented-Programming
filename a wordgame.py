1fbb6faa9805715655336bd6180c70d6bec893bb
rayrocket07 <61902415+rayrocket07@users.noreply.github.com>
2020-03-08 18:22:42 +0700

    Update a wordgame.py

--- a wordgame.py
+++ a wordgame.py
@@ -28,6 +28,29 @@ def getWordScore(word, n):
     if len(word) == n:
         extra = 50
     return(totalP + extra)
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    hi = {}
+    for key,value in hand.items():
+        hi[key] = value
+    for i in word:
+        hi[i] -= 1
+    return(hi)
+    
 
                
                    
