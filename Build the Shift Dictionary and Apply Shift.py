f51b0f38411e70213cca13c9da48d8a3bbba308a
rayrocket07 <61902415+rayrocket07@users.noreply.github.com>
2020-03-08 18:08:29 +0700

    Update Build the Shift Dictionary and Apply Shift.py

--- Build the Shift Dictionary and Apply Shift.py
+++ Build the Shift Dictionary and Apply Shift.py
@@ -94,3 +94,67 @@ class Message(object):
                 newscript += i
         return(newscript)
 
+class PlaintextMessage(Message):
+    def __init__(self, text, shift):
+        '''
+        Initializes a PlaintextMessage object        
+        
+        text (string): the message's text
+        shift (integer): the shift associated with this message
+
+        A PlaintextMessage object inherits from Message and has five attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+            self.shift (integer, determined by input shift)
+            self.encrypting_dict (dictionary, built using shift)
+            self.message_text_encrypted (string, created using shift)
+
+        Hint: consider using the parent class constructor so less 
+        code is repeated
+        '''
+        Message.__init__(self, text)
+        self.nshift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, self.nshift)
+        self.message_text_encrypted = Message.apply_shift(self, self.nshift)
+
+    def get_shift(self):
+        '''
+        Used to safely access self.shift outside of the class
+        
+        Returns: self.shift
+        '''
+        return self.nshift
+
+    def get_encrypting_dict(self):
+        '''
+        Used to safely access a copy self.encrypting_dict outside of the class
+        
+        Returns: a COPY of self.encrypting_dict
+        '''
+        newdict = {}
+        for k,y in self.encrypting_dict.items():
+            newdict[k] = y
+        return newdict
+
+    def get_message_text_encrypted(self):
+        '''
+        Used to safely access self.message_text_encrypted outside of the class
+        
+        Returns: self.message_text_encrypted
+        '''
+        return self.message_text_encrypted
+
+    def change_shift(self, shift):
+        '''
+        Changes self.shift of the PlaintextMessage and updates other 
+        attributes determined by shift (ie. self.encrypting_dict and 
+        message_text_encrypted).
+        
+        shift (integer): the new shift that should be associated with this message.
+        0 <= shift < 26
+
+        Returns: nothing
+        '''
+        self.nshift = shift
+        self.encrypting_dict = Message.build_shift_dict(self, self.nshift)
+        self.message_text_encrypted = Message.apply_shift(self, self.nshift)
